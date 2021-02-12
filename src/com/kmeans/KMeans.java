ad208ef512a21554afc13e24535f4a0f2312e865
Samuel Atkins <s.m.atkins73@gmail.com>
2021-02-11 18:18:32 -0800

    11/02/2021-Atkins Completed kMeans clustering algorithm. Just need to add time results to obtain complete results.

--- src/com/kmeans/KMeans.java
+++ src/com/kmeans/KMeans.java
@@ -1,39 +1,50 @@
 package com.kmeans;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.*;
 import java.io.IOException;
 
+import org.apache.avro.generic.GenericData;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapred.*;
 
 public class KMeans {
-    public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
-        private final static IntWritable one = new IntWritable(1);
-        private Text word = new Text();
-        public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException{
+    public static int k;
+    public static long dataFileLength;
+    public static List<List<Double>> centroidList = new ArrayList<>();
+
+    public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, DoubleWritable> {
+        public void map(LongWritable key, Text value, OutputCollector<Text, DoubleWritable> output, Reporter reporter) throws IOException{
             // mapper code
             String line = value.toString();
-            String pointStr[] = line.split(",");
-            List<Double> point = new ArrayList<>();
-            point.add(Double.valueOf(pointStr[0]));
-            point.add(Double.valueOf(pointStr[1]));
-
-            StringTokenizer tokenizer = new StringTokenizer(line);
-            while(tokenizer.hasMoreTokens()){
-                word.set(tokenizer.nextToken());
-                output.collect(word, one); // {Mary, 1}, {had, 1}, {a, 1}, {little, 1}, {lamb, 1}, {little, 1}, {lamb, 1}
+            List<Double> point = Helpers.getPointFromString(line);
+            double minDistance = -1f;
+            int minIndex = -1;
+            for (int i = 0; i < KMeans.k; i++){
+                double distance = Helpers.getEuclideanDistance(point, KMeans.centroidList.get(i));
+                if (minDistance == -1f || distance < minDistance){
+                    minDistance = distance;
+                    minIndex = i;
+                }
             }
+            output.collect(new Text(String.valueOf(minIndex) + "x"), new DoubleWritable(point.get(0)));
+            output.collect(new Text(String.valueOf(minIndex) + "y"), new DoubleWritable(point.get(1)));
         }
     }
 
-    public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
-        public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException{
+    public static class Reduce extends MapReduceBase implements Reducer<Text, DoubleWritable, Text, DoubleWritable> {
+        public void reduce(Text key, Iterator<DoubleWritable> values, OutputCollector<Text, DoubleWritable> output, Reporter reporter) throws IOException {
             // reducer code
             // Mary: {1}, had: {1}, a: {1}, little: {1, 1}, lamb: {1, 1}
-            int sum = 0;
-            while(values.hasNext()){
+            int counter = 0;
+            Double sum = 0d;
+
+            while (values.hasNext()) {
                 sum += values.next().get();
+                counter += 1;
             }
-            output.collect(key, new IntWritable(sum));
+            output.collect(key, new DoubleWritable(sum/counter));
         }
     }
 }
